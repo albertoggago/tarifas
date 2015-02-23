b7fa9b2e90c4ffb4eb688b8a74162a3f1a27bdc2
Alberto J Garcia Gago <albertoggago@gmail.com>
2015-02-23 21:21:06 +0100

    Ver 0.4.01

--- src/tarifasApp.js
+++ src/tarifasApp.js
@@ -88,145 +88,128 @@ tarifasApp.controller("tarifasListaController", function($scope, $http) {
 		 for (var i = 0; i < $scope.datos.tabla.length; ++i)
 	    	{
 	    	 //recogemos los datos y los actualizamos
-			 $scope.datos.tabla[i].gasto_minutos   = parseFloat($scope.datos.minutos);
-			 $scope.datos.tabla[i].gasto_llamadas  = parseFloat($scope.datos.llamadas); 
-			 $scope.datos.tabla[i].gasto_sms       = parseFloat($scope.datos.sms);
-			 $scope.datos.tabla[i].gasto_internet  = parseFloat($scope.datos.internet);
+			 gasto_minutos   = parseFloat($scope.datos.minutos);
+			 gasto_llamadas  = parseFloat($scope.datos.llamadas); 
+			 gasto_sms       = parseFloat($scope.datos.sms);
+			 gasto_internet  = parseFloat($scope.datos.internet);
 
-			 //var coste_minutos   = parseFloat($scope.datos.tabla[i][5]);
-			 //var coste_llamadas  = parseFloat($scope.datos.tabla[i][6]);
-			 //var coste_sms       = parseFloat($scope.datos.tabla[i][7]);
-			 //var coste_internet  = parseFloat($scope.datos.tabla[i][8]);
-			 //var tarifa_std      = parseFloat($scope.datos.tabla[i][9]);
+			 var coste_minutos   = parseFloat($scope.datos.tabla[i].coste_minutos);
+			 var coste_llamadas  = parseFloat($scope.datos.tabla[i].coste_llamadas);
+			 var coste_sms       = parseFloat($scope.datos.tabla[i].coste_sms);
+			 var coste_internet  = parseFloat($scope.datos.tabla[i].coste_internet);
+			 var tarifa_std      = parseFloat($scope.datos.tabla[i].tarifa_std);
 
-			 //var tarifa_minima   = parseFloat($scope.datos.tabla[i][10]);
+			 var tarifa_minima   = parseFloat($scope.datos.tabla[i].tarifa_minima);
 
-			 //var incluidos_minutos  = parseFloat($scope.datos.tabla[i][11]);
-			 //var incluidos_sms      = parseFloat($scope.datos.tabla[i][13]);
-			 //var incluidos_internet = parseFloat($scope.datos.tabla[i][14]);
+			 var incluidos_minutos  = parseFloat($scope.datos.tabla[i].incluidos_minutos);
+			 var incluidos_sms      = parseFloat($scope.datos.tabla[i].incluidos_sms);
+			 var incluidos_internet = parseFloat($scope.datos.tabla[i].incluidos_internet);
 
-			 //var coste_incluido_sn = $scope.datos.tabla[i][12].toString();
-			 //var sn_4G = ""
-			 //var sn_4G = $scope.datos.tabla[i][22];
-			 /*if (typeof($scope.datos.tabla[i][22]) != "undefined")
-			          {sn_4G    = $scope.datos.tabla[i][22].toString();}
-			      else{sn_4G = "NO";
-			      	$scope.datos.tabla[i][22] = sn_4G
-			      };*/
+			 var coste_incluido_sn = $scope.datos.tabla[i].coste_incluido_sn;
+			 var sn_4G = ""
+			 var sn_4G = $scope.datos.tabla[i].sn_4G;
 
 
 	    	 //actualizamos el Gasto
-			 //$scope.datos.tabla[i][15]= gasto_minutos;
-			 //$scope.datos.tabla[i][16]= gasto_llamadas; 
-			 //$scope.datos.tabla[i][17]= gasto_sms;
-			 //$scope.datos.tabla[i][18]= gasto_internet; 
-
-
-                
+			 $scope.datos.tabla[i].gasto_minutos= gasto_minutos;
+			 $scope.datos.tabla[i].gasto_llamadas= gasto_llamadas; 
+			 $scope.datos.tabla[i].gasto_sms= gasto_sms;
+			 $scope.datos.tabla[i].gasto_internet= gasto_internet; 
 
 
 		     //cáclulo, lo primero miramos si hemos consumido mas llamadas que las incluidas.
-		     var var_tarifa= 0;
-		     var var_minutos_pagar = 0;
-		     if ($scope.datos.tabla[i].gasto_minutos>$scope.datos.tabla[i].incluidos_minutos) {
-		    	 var_minutos_pagar = 
-                     $scope.datos.tabla[i].gasto_minutos-$scope.datos.tabla[i].incluidos_minutos
+		     var tarifa= 0;
+		     var minutos_pagar = 0;
+		     if (gasto_minutos>incluidos_minutos) {
+		    	 minutos_pagar = gasto_minutos-incluidos_minutos
 		     };
          	 // Incluimos el coste de los minutos no incluidos. 
-		     var_tarifa += var_minutos_pagar * $scope.datos.tabla[i].coste_minutos / 100
+		     tarifa += minutos_pagar * coste_minutos / 100
 		     //calculos Coste de LLamada Incluido o no.
-		     var var_llamadas_pagar = 0;
-		     if ($scope.datos.tabla[i].coste_incluido_sn == "NO") {
-		    	 var_llamadas_pagar = $scope.datos.tabla[i].gasto_llamadas;
+             var llamadas_pagar = 0;
+		     if (coste_incluido_sn == "NO") {
+		    	 llamadas_pagar = gasto_llamadas;
 		     } else {
-		    	 if ($scope.datos.tabla[i].gasto_minutos>$scope.datos.tabla[i].incluidos_minutos) {
-		    		 var_llamadas_pagar = 
-                         ($scope.datos.tabla[i].gasto_minutos-$scope.datos.tabla[i].incluidos_minutos)* 
-                          $scope.datos.tabla[i].gasto_llamadas
-		    		 //var_llamadas_pagar = (gasto_minutos-incluidos_minutos)* gasto_llamadas
-		    		 if ($scope.datos.tabla[i].gasto_minutos!=0){
-		    			 var_llamadas_pagar = var_llamadas_pagar / $scope.datos.tabla[i].gasto_minutos;
+		    	 if (gasto_minutos>incluidos_minutos) {
+		    		 llamadas_pagar = (gasto_minutos-incluidos_minutos)* gasto_llamadas
+		    		 if (gasto_minutos!=0){
+		    			 llamadas_pagar = llamadas_pagar / gasto_minutos;
 		    		 	}
 		    	 	 }
 		     };
-		     var_tarifa += var_llamadas_pagar*$scope.datos.tabla[i].coste_llamadas/100.0;
-		     //calculamos Internet
-		     var var_sms_pagar = 0.0;
-		     if ($scope.datos.tabla[i].gasto_sms>$scope.datos.tabla[i].incluidos_sms) {
-		    	 var_sms_pagar = $scope.datos.tabla[i].gasto_sms-$scope.datos.tabla[i].incluidos_sms
+                
+		     tarifa += llamadas_pagar*coste_llamadas/100.0;
+		     
+		     //calculamos SMS
+		     var sms_pagar = 0.0;
+		     if (gasto_sms>incluidos_sms) {
+		    	 sms_pagar = gasto_sms-incluidos_sms
 		     };
-		     var_tarifa += var_sms_pagar*$scope.datos.tabla[i].coste_sms/100.0; 
-		     var var_internet_pagar = 0.0;
-		     if ($scope.datos.tabla[i].gasto_internet>$scope.datos.tabla[i].incluidos_internet) {
-		    	 var_internet_pagar = $scope.datos.tabla[i].gasto_internet-$scope.datos.tabla[i].incluidos_internet
+		     tarifa += sms_pagar*coste_sms/100.0; 
+		     var internet_pagar = 0.0;
+		     if (gasto_internet>incluidos_internet) {
+		    	 internet_pagar = gasto_internet-incluidos_internet;
 		     };
-             var var_sobrecoste_internet =  var_internet_pagar*$scope.datos.tabla[i].coste_internet/100.0;
-		     var_tarifa += var_sobrecoste_internet;		    	 
+             var sobrecoste_internet = internet_pagar*coste_internet/100.0;
+		     tarifa += sobrecoste_internet;		                          
+             sobrecoste_internet *= $scope.datosSTD.IVA;
 		     
 		     //
 		     //=max(P3-L3;0)*F3/100+
 		     // if(M3<>"NO";max(P3-L3;0)*Q3/if(P3<>0;P3;1);Q3)*G3/100+
 		     // max(R3-N3;0)*H3/100+
 		     // max(S3-O3;0)*I3/100
-		     var var_total_base= Math.round(var_tarifa*100.0)/100;
+		     var total_base= Math.round(tarifa*100.0)/100;
 		     
 		      //CALCULOS ESPECIALES..... NO HACEMOS NADA
-		     var var_calculos_especiales = 0;
+		     var calculos_especiales = 0;
 		     
 		     
 		     //calculamos el Total + mínimos.
-		     var var_total_minimos = 0;
-		     if (var_total_base>$scope.datos.tabla[i].tarifa_minima){
-		    	 var_total_minimos = var_total_base;
+		     var total_minimos = 0;
+		     if (total_base>tarifa_minima){
+		    	 total_minimos = total_base;
 		     } else {
-		    	 var_total_minimos = $scope.datos.tabla[i].tarifa_minima;
+		    	 total_minimos = tarifa_minima;
 		     };
-		     var var_total_sin_IVA = Math.round((
-                 var_total_minimos+$scope.datos.tabla[i].tarifa_std+var_calculos_especiales)*100.0)/100;
+		     total_sin_IVA = Math.round((total_minimos+tarifa_std+calculos_especiales)*100.0)/100;
 		     //=max(U3;K3)+J3+T3
 		     //calculamos el precio con IVA
-		     var var_total_con_IVA  = Math.round(var_total_sin_IVA*$scope.datosSTD.IVA*100.0)/100.0;
+		     var total_con_IVA  = Math.round(total_sin_IVA*$scope.datosSTD.IVA*100.0)/100.0;
 
 		     
 		     //textos especiales
-		     var var_textos_especiales = "";
+		     textos_especiales = "";
 		     //texto Tarifa Superada
-		     if ($scope.datos.tabla[i].coste_internet == 0 && 
-                ($scope.datos.tabla[i].gasto_internet > $scope.datos.tabla[i].incluidos_internet &&
-                 $scope.datos.tabla[i].incluidos_internet != 0)){
-		    	 var_textos_especiales +=  "Superada Tarifa Internet: "
-                 +$scope.numEditImpStd(($scope.datos.tabla[i].gasto_internet-
-                                        $scope.datos.tabla[i].incluidos_internet)/
-                                       $scope.datos.tabla[i].gasto_internet*30)
+		     if (coste_internet == 0 && (gasto_internet > incluidos_internet && incluidos_internet != 0)){
+		    	 textos_especiales +=  "Superada Tarifa Internet: "+$scope.numEditImpStd((gasto_internet-incluidos_internet)/gasto_internet*30)
 		    	                      +" días del mes a baja velocidad"; 
 		     } 
-		     if ($scope.datos.tabla[i].coste_internet != 0 && 
-                 ($scope.datos.tabla[i].gasto_internet > $scope.datos.tabla[i].incluidos_internet)){
-		    	 var_textos_especiales += "Incluido Sobrecoste por Datos de "
-                        +$scope.numEditImpStd(var_sobrecoste_internet)+" Euros"; 
+		     if (coste_internet != 0 && (gasto_internet > incluidos_internet)){
+		    	 textos_especiales += "Incluido Sobrecoste por Datos de "+$scope.numEditImpStd(sobrecoste_internet)+" Euros"; 
 		     } 
-		     if ($scope.datos.tabla[i].sn_4G == "SI" ){
-		    	 var_textos_especiales += " - Con 4G"; 
+		     if (sn_4G == "SI" ){
+		    	 textos_especiales += " - Con 4G"; 
 		     } 
 		     
-		     if (var_textos_especiales == "") {var_textos_especiales = "bbbb";}
-		     
-		     //=concatenate(
-		     //if(and(I3=0;O3<S3);concatenate("Superada Tarifa Internet: ";fixed((S3-O3)/S3*30;2);" días del mes a baja velocidad");
-		     //if(O3<S3;concatenate("Incluido Sobrecoste por Datos de ";Fixed((S3-O3)*I3*1,21/100;2);" Euros");""));
-		     //if(W3="SI";" - Con 4G";"bbbb"))
+		     //if (textos_especiales == "") {textos_especiales = "bbbb";}    
+             
+             if ($scope.datos.observaciones== "bbbb") {$scope.datos.observaciones== ""} 
+
 
 			 //Movemos los calculos
-			 $scope.datos.tabla[i].total_con_IVA  = var_total_con_IVA;
-			 $scope.datos.tabla[i].textos_especiales  = var_textos_especiales;
+			 $scope.datos.tabla[i].total_con_IVA  = total_con_IVA;
+			 $scope.datos.tabla[i].textos_especiales  = textos_especiales;
 			 $scope.datos.tabla[i].formulas_especiales = "Sin datos actualmente, pendiente";
-			 $scope.datos.tabla[i].total_base = var_total_base;
-			 $scope.datos.tabla[i].total_sin_IVA = var_total_sin_IVA;
+			 $scope.datos.tabla[i].total_base = total_base;
+			 $scope.datos.tabla[i].total_sin_IVA = total_sin_IVA;
+             $scope.datos.tabla[i].sobrecoste_internet = sobrecoste_internet;
+             
 		     
 	    };
 	    //funcion de ordenar
 	    // ya no usamos esto    
-	      $scope.datos.tabla.sort(function(a,b){return parseFloat(a[2])-parseFloat(b[2])});
+	   //$scope.datos.tabla.sort(function(a,b){return parseFloat(a[2])-parseFloat(b[2])});
 		
 	};    
     
@@ -259,13 +242,13 @@ tarifasApp.controller("tarifasListaController", function($scope, $http) {
 		this.setInternet(tab[0].gasto_internet);
 		$scope.datos.tabla = tab;
 		//recalculamos siempre que se realiza una carga para ajustarlo todo
-		//$scope.actualizarTarifas();
+		$scope.actualizarTarifas();
 		$scope.datos.version     = versionX;
 		$scope.determinarFecha();
 	};
 
     
-    this.getPrecios   = function() {
+/*    $scope.getPrecios   = function() {
 		var lista = [];
 		for (var i=0; i < $scope.datos.tabla.length; i++) {
 			lista.push(parseFloat($scope.datos.tabla[i][2]));
@@ -280,7 +263,7 @@ tarifasApp.controller("tarifasListaController", function($scope, $http) {
 		else{
 			return ""
 		};
-	};
+	};*/
 
 	this.actualizar = function (eMin, eLlam, eSms, eMB){
 		$scope.datos.minutos = eMin;
--- test/unit/tarifasSpec.js
+++ test/unit/tarifasSpec.js
@@ -34,11 +34,19 @@ describe('tarifasAppControl', function() {
        expect(scope.numEdit(-123456789.128456,2)).toBe("-123.456.789,13");                        
     });
 
-      var x1 = ["x1","Orange",24.9502,"60 minutos de IP","- Con 4G",15,16,17,18,19,20,21,"SI",1000,2000,0,0,0,0,0,20.62,33,"SI"];
-	  var y1 = ["y1","Orange",24.9502,"60 minutos de IP","- Con 4G",15,16,17,18,19,20,21,"NO",1000,2000,0,0,0,0,0,20.62,33,"NO"];
-	  var z1 = ["z1","Orange",24.9502,"60 minutos de IP","- Con 4G",25,26,17,18,19,20,21,"NO",1000,2000,0,0,0,0,0,20.62,33,"NO"];
-	  var x9 =  ["Yoigo Infinita 39","Yoigo+Movistar",0,"bbbb"," - Con 4G",0,0,10,0,32.23,0,99999,"SI",0,4000,1000,150,75,5000,0,0,0,"SI"];
-	  var tab1 = [x1,y1];
+     // var x1 = ["x1","Orange",24.9502,"60 minutos de IP","- Con 4G",15,16,17,18,19,20,21,"SI",1000,2000,0,0,0,0,0,20.62,33,"SI"];
+	  //var y1 = ["y1","Orange",24.9502,"60 minutos de IP","- Con 4G",15,16,17,18,19,20,21,"NO",1000,2000,0,0,0,0,0,20.62,33,"NO"];
+	  //var z1 = ["z1","Orange",24.9502,"60 minutos de IP","- Con 4G",25,26,17,18,19,20,21,"NO",1000,2000,0,0,0,0,0,20.62,33,"NO"];
+	  //var x9 =  ["Yoigo Infinita 39","Yoigo+Movistar",0,"bbbb"," - Con 4G",0,0,10,0,32.23,0,99999,"SI",0,4000,1000,150,75,5000,0,0,0,"SI"];
+    
+    
+    var x1 = {"nombre":"x1", "red":"Orange", "total_con_IVA":24.9502, "observaciones":" 60 minutos de IP", "textos_especiales":" - Con 4G", "coste_minutos":15, "coste_llamadas":16, "coste_sms":17, "coste_internet":18, "tarifa_std":19, "tarifa_minima":20, "incluidos_minutos":21,"coste_incluido_sn":"SI",  "incluidos_sms":1000, "incluidos_internet":2000, "gasto_minutos":0, "gasto_llamadas":0, "gasto_sms":0, "gasto_internet":0, "total_base":20.62, "total_sin_IVA":63.33,"formulas_especiales":"Sin datos actualmente, pendiente","sn_4G":"SI"};
+    var y1 = {"nombre":"y1", "red":"Orange", "total_con_IVA":24.9502, "observaciones":" 60 minutos de IP", "textos_especiales":" - Con 4G", "coste_minutos":15, "coste_llamadas":16, "coste_sms":17, "coste_internet":18, "tarifa_std":19, "tarifa_minima":20, "incluidos_minutos":21,"coste_incluido_sn":"NO",  "incluidos_sms":1000, "incluidos_internet":2000, "gasto_minutos":0, "gasto_llamadas":0, "gasto_sms":0, "gasto_internet":0, "total_base":20.62, "total_sin_IVA":63.33,"formulas_especiales":"Sin datos actualmente, pendiente","sn_4G":"NO"};
+	var z1 = {"nombre":"y1", "red":"Orange", "total_con_IVA":24.9502, "observaciones":" 60 minutos de IP", "textos_especiales":" - Con 4G", "coste_minutos":25, "coste_llamadas":26, "coste_sms":17, "coste_internet":18, "tarifa_std":19, "tarifa_minima":20, "incluidos_minutos":21,"coste_incluido_sn":"NO",  "incluidos_sms":1000, "incluidos_internet":2000, "gasto_minutos":0, "gasto_llamadas":0, "gasto_sms":0, "gasto_internet":0, "total_base":20.62, "total_sin_IVA":63.33,"formulas_especiales":"Sin datos actualmente, pendiente","sn_4G":"NO"};
+    var x9 = {"nombre":"Yoigo Infinita 39", "red":"Yoigo+Movistar", "total_con_IVA":24.9502, "observaciones":"bbbb", "textos_especiales":" - Con 4G", "coste_minutos":0, "coste_llamadas":0, "coste_sms":10, "coste_internet":0, "tarifa_std":32.23, "tarifa_minima":0, "incluidos_minutos":99999,"coste_incluido_sn":"SI",  "incluidos_sms":0, "incluidos_internet":4000, "gasto_minutos":1000, "gasto_llamadas":150, "gasto_sms":75, "gasto_internet":5000, "total_base":0, "total_sin_IVA":0,"formulas_especiales":"Sin datos actualmente, pendiente","sn_4G":"NO"};
+	  
+    
+        var tab1 = [x1,y1];
 
     
     it("Revision de Precios0_66666_7777_6666", function() { 
@@ -48,141 +56,132 @@ describe('tarifasAppControl', function() {
   		 expect(ctrl.getLlamadas()).toBe(8888);
   		 expect(ctrl.getSMS()).toBe(7777);
   		 expect(ctrl.getInternet()).toBe(6666);
-  		 expect(scope.datos.tabla[0][12]).toBe("SI");
-  		 expect(scope.datos.tabla[1][12]).toBe("NO");
-  		 expect(scope.datos.tabla[0][15]).toBe(9999);
-  		 expect(scope.datos.tabla[1][15]).toBe(9999);
-  		 expect(scope.datos.tabla[0][16]).toBe(8888);
-  		 expect(scope.datos.tabla[1][16]).toBe(8888);
-  		 expect(scope.datos.tabla[0][17]).toBe(7777);
-  		 expect(scope.datos.tabla[1][17]).toBe(7777);
-  		 expect(scope.datos.tabla[0][18]).toBe(6666);
-  		 expect(scope.datos.tabla[1][18]).toBe(6666);
+  		 expect(scope.datos.tabla[0].coste_incluido_sn).toBe("SI");
+  		 expect(scope.datos.tabla[1].coste_incluido_sn).toBe("NO");
+  		 expect(scope.datos.tabla[0].gasto_minutos).toBe(9999);
+  		 expect(scope.datos.tabla[1].gasto_minutos).toBe(9999);
+  		 expect(scope.datos.tabla[0].gasto_llamadas).toBe(8888);
+  		 expect(scope.datos.tabla[1].gasto_llamadas).toBe(8888);
+  		 expect(scope.datos.tabla[0].gasto_sms).toBe(7777);
+  		 expect(scope.datos.tabla[1].gasto_sms).toBe(7777);
+  		 expect(scope.datos.tabla[0].gasto_internet).toBe(6666);
+  		 expect(scope.datos.tabla[1].gasto_internet).toBe(6666);
  	 });
 
     it("Revision de Precios0_0_0_0", function() {
-        ctrl.setTabla([x1.slice(),y1.slice()]);
+        ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
   	    ctrl.actualizar(0,0,0,0);
-        expect(scope.datos.tabla[0][20]).toBe(0);
-  		expect(scope.datos.tabla[1][20]).toBe(0);
-  		expect(scope.datos.tabla[0][21]).toBe(39);
-  		expect(scope.datos.tabla[1][21]).toBe(39);
-  		expect(ctrl.getPrecio(0)).toBe(47.19);
-  		expect(ctrl.getPrecio(1)).toBe(47.19);
-  		expect(scope.datos.tabla[0][4]).toBe(" - Con 4G");
-  		expect(scope.datos.tabla[1][4]).toBe("bbbb");
+        expect(scope.datos.tabla[0].total_base).toBe(0);
+  		expect(scope.datos.tabla[1].total_base).toBe(0);
+  		expect(scope.datos.tabla[0].total_sin_IVA).toBe(39);
+  		expect(scope.datos.tabla[1].total_sin_IVA).toBe(39);
+  		expect(scope.datos.tabla[0].total_con_IVA).toBe(47.19);
+  		expect(scope.datos.tabla[1].total_con_IVA).toBe(47.19);
+  		expect(scope.datos.tabla[0].textos_especiales).toBe(" - Con 4G");
+  		expect(scope.datos.tabla[1].textos_especiales).toBe("");
  	 });
 
 
   
  	 it("Revision de Precios9999_0_0_0", function() {
-     	 ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+     	 ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
     	 ctrl.actualizar(9999,0,0,0);
-		 expect(scope.datos.tabla[0][20]).toBe(1496.70);
-	 	 expect(scope.datos.tabla[1][20]).toBe(1496.70);
-		 expect(scope.datos.tabla[0][21]).toBe(1515.70);
-	 	 expect(scope.datos.tabla[1][21]).toBe(1515.70);
-  		 expect(ctrl.getPrecio(0)).toBe(1834);
-  		 expect(ctrl.getPrecio(1)).toBe(1834);
-  		 expect(scope.datos.tabla[0][4]).toBe(" - Con 4G");
-  		 expect(scope.datos.tabla[1][4]).toBe("bbbb");
+		 expect(scope.datos.tabla[0].total_base).toBe(1496.70);
+	 	 expect(scope.datos.tabla[1].total_base).toBe(1496.70);
+		 expect(scope.datos.tabla[0].total_sin_IVA).toBe(1515.70);
+	 	 expect(scope.datos.tabla[1].total_sin_IVA).toBe(1515.70);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(1834);
+  		 expect(scope.datos.tabla[1].total_con_IVA).toBe(1834);
+  		 expect(scope.datos.tabla[0].textos_especiales).toBe(" - Con 4G");
+  		 expect(scope.datos.tabla[1].textos_especiales).toBe("");
  	 });	
  	 
 	 it("Revision de Precios9999_8888_0_0", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+         ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
  	     ctrl.actualizar(9999,8888,0,0);
-	     expect(scope.datos.tabla[0][20]).toBe(2915.79);
-	 	 expect(scope.datos.tabla[1][20]).toBe(2918.78);
-	 	 expect(scope.datos.tabla[0][21]).toBe(2934.79);
-	 	 expect(scope.datos.tabla[1][21]).toBe(2937.78);
-  		 expect(ctrl.getPrecio(0)).toBe(3551.10);
-  		 expect(ctrl.getPrecio(1)).toBe(3554.71);
-  		 expect(scope.datos.tabla[0][4]).toBe(" - Con 4G");
-  		 expect(scope.datos.tabla[1][4]).toBe("bbbb");
+	     expect(scope.datos.tabla[0].total_base).toBe(2915.79);
+	 	 expect(scope.datos.tabla[1].total_base).toBe(2918.78);
+	 	 expect(scope.datos.tabla[0].total_sin_IVA).toBe(2934.79);
+	 	 expect(scope.datos.tabla[1].total_sin_IVA).toBe(2937.78);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(3551.10);
+  		 expect(scope.datos.tabla[1].total_con_IVA).toBe(3554.71);
+  		 expect(scope.datos.tabla[0].textos_especiales).toBe(" - Con 4G");
+  		 expect(scope.datos.tabla[1].textos_especiales).toBe("");
 	 });
 
 	 it("Revision de Precios9999_8888_7777_0", function() {
-     	 ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+     	 ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
     	 ctrl.actualizar(9999,8888,7777,0);
-    	 expect(scope.datos.tabla[0][20]).toBe(4067.88);
-	 	 expect(scope.datos.tabla[1][20]).toBe(4070.87);
-	 	 expect(scope.datos.tabla[0][21]).toBe(4086.88);
-	 	 expect(scope.datos.tabla[1][21]).toBe(4089.87);
-  		 expect(ctrl.getPrecio(0)).toBe(4945.12);
-  		 expect(ctrl.getPrecio(1)).toBe(4948.74);
-  		 expect(scope.datos.tabla[0][4]).toBe(" - Con 4G");
-  		 expect(scope.datos.tabla[1][4]).toBe("bbbb");
+    	 expect(scope.datos.tabla[0].total_base).toBe(4067.88);
+	 	 expect(scope.datos.tabla[1].total_base).toBe(4070.87);
+	 	 expect(scope.datos.tabla[0].total_sin_IVA).toBe(4086.88);
+	 	 expect(scope.datos.tabla[1].total_sin_IVA).toBe(4089.87);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(4945.12);
+  		 expect(scope.datos.tabla[1].total_con_IVA).toBe(4948.74);
+  		 expect(scope.datos.tabla[0].textos_especiales).toBe(" - Con 4G");
+  		 expect(scope.datos.tabla[1].textos_especiales).toBe("");
 	 });
 
 
 
 	 it("Revision de Precios9999_8888_7777_6666", function() {
-    	 ctrl.setTabla([x1.slice(),y1.slice()]);
+    	 ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
   	     ctrl.actualizar(9999,8888,7777,6666);
-         expect(scope.datos.tabla[0][20]).toBe(4907.76);
-	 	 expect(scope.datos.tabla[1][20]).toBe(4910.75);
-	 	 expect(scope.datos.tabla[0][21]).toBe(4926.76);
-	 	 expect(scope.datos.tabla[1][21]).toBe(4929.75);
-  		 expect(ctrl.getPrecio(0)).toBe(5961.38);
-  		 expect(ctrl.getPrecio(1)).toBe(5965.00);
-  		 expect(scope.datos.tabla[0][4]).toBe("Incluido Sobrecoste por Datos de 1.016,25 Euros - Con 4G");
-  		 expect(scope.datos.tabla[1][4]).toBe("Incluido Sobrecoste por Datos de 1.016,25 Euros");
+         expect(scope.datos.tabla[0].total_base).toBe(4907.76);
+	 	 expect(scope.datos.tabla[1].total_base).toBe(4910.75);
+	 	 expect(scope.datos.tabla[0].total_sin_IVA).toBe(4926.76);
+	 	 expect(scope.datos.tabla[1].total_sin_IVA).toBe(4929.75);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(5961.38);
+  		 expect(scope.datos.tabla[1].total_con_IVA).toBe(5965.00);
+  		 expect(scope.datos.tabla[0].textos_especiales).toBe("Incluido Sobrecoste por Datos de 1.016,25 Euros - Con 4G");
+  		 expect(scope.datos.tabla[1].textos_especiales).toBe("Incluido Sobrecoste por Datos de 1.016,25 Euros");
 	 });
 
 	 it("Revision de Precios0010_0011_0012_0013", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()]);
+         ctrl.setTabla([JSON.parse(JSON.stringify(x1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
  	     ctrl.actualizar(10,11,12,13);
-	 	 expect(scope.datos.tabla[0][20]).toBe(0.00);
-	 	 expect(scope.datos.tabla[1][20]).toBe(1.76);
-	 	 expect(scope.datos.tabla[0][21]).toBe(39); 
-	 	 expect(scope.datos.tabla[1][21]).toBe(39);
-  		 expect(ctrl.getPrecio(0)).toBe(47.19);
-  		 expect(ctrl.getPrecio(1)).toBe(47.19);
-  		 expect(scope.datos.tabla[0][4]).toBe(" - Con 4G");
-  		 expect(scope.datos.tabla[1][4]).toBe("bbbb");
+	 	 expect(scope.datos.tabla[0].total_base).toBe(0.00);
+	 	 expect(scope.datos.tabla[1].total_base).toBe(1.76);
+	 	 expect(scope.datos.tabla[0].total_sin_IVA).toBe(39); 
+	 	 expect(scope.datos.tabla[1].total_sin_IVA).toBe(39);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(47.19);
+  		 expect(scope.datos.tabla[1].total_con_IVA).toBe(47.19);
+  		 expect(scope.datos.tabla[0].textos_especiales).toBe(" - Con 4G");
+  		 expect(scope.datos.tabla[1].textos_especiales).toBe("");
 	 });
 	 
-	 it("Revision de Precios9999_8888_7777_6666_2", function() {
-         ctrl.setTabla([z1.slice(),y1.slice(),x1.slice()]);
-         ctrl.actualizar(9999,8888,7777,6666);
-		 expect(scope.datos.tabla[0][0]).toBe("x1");
-		 expect(scope.datos.tabla[1][0]).toBe("y1");
-		 expect(scope.datos.tabla[2][0]).toBe("z1");
-  		 expect(ctrl.getPrecio(0)).toBe(5961.38);
-  		 expect(ctrl.getPrecio(1)).toBe(5965.00);
-  		 expect(ctrl.getPrecio(2)).toBe(8247.78);
-	 });
-
+//queda pendiente una prueva de ordenacion
+    
 	 it("Yoigo Infinita 39", function() {
-         ctrl.setTabla([x9]);  
+         ctrl.setTabla([JSON.parse(JSON.stringify(x9))],scope.datosSTD.VERSION);
          ctrl.actualizar(1000,150,75,5000);
-         expect(scope.datos.tabla[0][7]).toBe(10);
-	 	 expect(scope.datos.tabla[0][20]).toBe(7.50);
-  		 expect(ctrl.getPrecio(0)).toBe(48.07);
+         expect(scope.datos.tabla[0].coste_sms).toBe(10);
+	 	 expect(scope.datos.tabla[0].total_base).toBe(7.50);
+  		 expect(scope.datos.tabla[0].total_con_IVA).toBe(48.07);
 	 });
 
  	 it("Verficar Version", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+         ctrl.setTabla([JSON.parse(JSON.stringify(z1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
          expect(ctrl.verificarVF(scope.datosSTD.VERSION)).toBe(true);
 	 	 expect(ctrl.verificarVF("VERSION")).toBe(false);
 	 });
 
  	 it("Verficar Fecha1", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+         ctrl.setTabla([JSON.parse(JSON.stringify(z1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
          scope.datos.fecha.year = "2000";
          expect(ctrl.verificarVF(scope.datosSTD.VERSION)).toBe(false);
 	 	 expect(ctrl.verificarVF("VERSION")).toBe(false);
 		 });
 
  	 it("Verficar Fecha1", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()],scope.datosSTD.VERSION);
+         ctrl.setTabla([JSON.parse(JSON.stringify(z1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
          scope.datos.fecha.month += 1;
          expect(ctrl.verificarVF(scope.datosSTD.VERSION)).toBe(false);
 	 	 expect(ctrl.verificarVF("VERSION")).toBe(false);
 		 });
 
  	 it("Verficar Fecha1", function() {
-         ctrl.setTabla([x1.slice(),y1.slice()]);
+         ctrl.setTabla([JSON.parse(JSON.stringify(z1)),JSON.parse(JSON.stringify(y1))],scope.datosSTD.VERSION);
          scope.datos.fecha.day -= 1;
 	 	 expect(ctrl.verificarVF(scope.datosSTD.VERSION)).toBe(false);
 	 	 expect(ctrl.verificarVF("VERSION")).toBe(false);
