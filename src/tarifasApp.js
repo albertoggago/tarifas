2b21ccf19de471659b64eb19dd12e8e584f2065f
Alberto J Garcia Gago <albertoggago@gmail.com>
2015-03-06 21:12:59 +0100

    Ver 0.6.05

--- src/tarifasApp.js
+++ src/tarifasApp.js
@@ -6,7 +6,7 @@ tarifasApp.controller("tarifasListaController", function($scope, $http, $interva
         "DECIMALPOINT" : ',',
         "SEPARADORMILES" : '.',
         "NUMERODECIMALES" : 2,
-        "VERSION" : "v00.05",
+        "VERSION" : "v00.06",
         "FICHERO" : "precios.json",
         "minutosSTD": 200,
         "llamadasSTD": 50,
